68ad1d3d6c1556c8524ff9b5137f8a1efbd959a7
tyhui <tyhuis34@gmail.com>
2024-12-16 08:14:42 -0500

    fiddling with webtorrent player

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.get('/favs', (req, res) => {
 
 // page for streaming media
 app.get('/stream', (req, res) => {
-  const magnet_link = "HagFFsasHJ547IFuGFU%64tFu%$TDasdFGaKJOdIaUH9";
+  const magnet_link = "magnet:?xt=urn:btih:2150DBD1E4CD9B64F217D78AEDB71116B3C098E0&dn=Gladiator%202%202024%201080p%20WEB-DL%20ENGLiSH%20x264-BAUCKLEY&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce&tr=udp%3A%2F%2Ftracker.bittor.pw%3A1337%2Fannounce&tr=udp%3A%2F%2Fpublic.popcorn-tracker.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.dler.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fexodus.desync.com%3A6969&tr=udp%3A%2F%2Fopen.demonii.com%3A1337%2Fannounce";
   res.render('stream', { magnet: magnet_link });
 });
 
@@ -64,14 +64,18 @@ app.get('/torrent_id', (req, res) => {
   });
 
   client.add(torrentMagnet, (torrent) => {
-    // Assuming the first file is the video file
-    const videoFile = torrent.files[0];
-    
-    // Set appropriate headers for streaming video
-    res.setHeader('Content-Type', videoFile.mime);
+    // Find the video file (filter out non-video files)
+    const videoFile = torrent.files.find(file => file.name.match(/\.(mp4|webm|mkv)$/i)); // Choose mp4, webm, or mkv
+
+    if (!videoFile) {
+      return res.status(404).send('Video file not found in the torrent.');
+    }
+
+    // Set appropriate headers for streaming the video
+    res.setHeader('Content-Type', videoFile.mime || 'video/mp4'); // Default to video/mp4 if MIME is not provided
     res.setHeader('Content-Length', videoFile.length);
 
-    // Pipe the file content to the response (streaming the video)
+    // Pipe the video file to the response (streaming the video)
     videoFile.createReadStream().pipe(res); 
   });
 });
