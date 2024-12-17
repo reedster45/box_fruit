62015c80012cf1cb11bd17f256b01b6f2bc1942b
Tyhuis <reed.tyhuis@KingstonHSC.ca>
2024-12-17 02:22:43 -0500

    encode and decode magnet link

--- app.js
+++ app.js
@@ -47,25 +47,23 @@ app.get('/favs', (req, res) => {
 // page for streaming media
 app.get('/stream', (req, res) => {
   const magnet_link = "magnet:?xt=urn:btih:2150DBD1E4CD9B64F217D78AEDB71116B3C098E0&dn=Gladiator%202%202024%201080p%20WEB-DL%20ENGLiSH%20x264-BAUCKLEY&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce&tr=udp%3A%2F%2Ftracker.bittor.pw%3A1337%2Fannounce&tr=udp%3A%2F%2Fpublic.popcorn-tracker.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.dler.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fexodus.desync.com%3A6969&tr=udp%3A%2F%2Fopen.demonii.com%3A1337%2Fannounce";
-  res.render('stream', { magnet: magnet_link });
+  const encoded_magnet_link = encodeURIComponent(magnet_link);
+  res.render('stream', { magnet: encoded_magnet_link });
 });
 
 // Stream the video via WebTorrent
 app.get('/torrent_id', (req, res) => {
-  const torrentMagnet = req.query.magnet;
+  const encoded_magent_link = req.query.magnet;
+  const magnet_link = decodeURIComponent(encoded_magent_link);
+  console.log(magnet_link);
   
-  if (!torrentMagnet) {
+  if (!magnet_link) {
     return res.status(400).send("Magnet link is required");
   }
 
-  // error listener for loading webtorrent magnet link
-  client.on('error', (err) => {
-    console.error('WebTorrent error:', err.message);
-  });
-
-  client.add(torrentMagnet, (torrent) => {
-    // Find the video file (filter out non-video files)
-    const videoFile = torrent.files.find(file => file.name.match(/\.(mp4|webm|mkv)$/i)); // Choose mp4, webm, or mkv
+  client.add(magnet_link, (torrent) => {
+    // Get the first file in the torrent (if there's more, you could handle it differently)
+    const videoFile = torrent.files[0];
 
     if (!videoFile) {
       return res.status(404).send('Video file not found in the torrent.');
@@ -77,6 +75,13 @@ app.get('/torrent_id', (req, res) => {
 
     // Pipe the video file to the response (streaming the video)
     videoFile.createReadStream().pipe(res); 
+  });
+
+
+  // Handle errors
+  client.on('error', (err) => {
+    console.error('WebTorrent error:', err);
+    res.status(500).send('An error occurred whle processing the magnet link');
   });
 });
 
