09ee8e4017bbc8755cea33a3c42325570e90d590
Tyhuis <reed.tyhuis@KingstonHSC.ca>
2024-12-17 02:49:22 -0500

    added load_database.py

--- app.js
+++ app.js
@@ -44,6 +44,12 @@ app.get('/favs', (req, res) => {
   res.render('favorites');
 });
 
+
+
+
+
+
+
 // page for streaming media
 app.get('/stream', (req, res) => {
   const magnet_link = "magnet:?xt=urn:btih:2150DBD1E4CD9B64F217D78AEDB71116B3C098E0&dn=Gladiator%202%202024%201080p%20WEB-DL%20ENGLiSH%20x264-BAUCKLEY&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce&tr=udp%3A%2F%2Ftracker.bittor.pw%3A1337%2Fannounce&tr=udp%3A%2F%2Fpublic.popcorn-tracker.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.dler.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fexodus.desync.com%3A6969&tr=udp%3A%2F%2Fopen.demonii.com%3A1337%2Fannounce";
@@ -84,6 +90,10 @@ app.get('/torrent_id', (req, res) => {
     res.status(500).send('An error occurred whle processing the magnet link');
   });
 });
+
+
+
+
 
 
 // listening on port 3000
---
+++ src/load_database.py
@@ -0,0 +1,56 @@
+import os
+import requests
+import gzip
+import shutil
+
+
+def download_unzip(url, output_folder):
+    # Download the file
+    response = requests.get(url, stream=True)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Get the name of the file from the URL
+        file_name = url.split('/')[-1]
+        gzipped_file_path = os.path.join(output_folder, file_name)
+        
+        # Save the gzipped content to a file
+        with open(gzipped_file_path, 'wb') as f:
+            f.write(response.content)
+        
+        # Unzip the gzipped file
+        with open(gzipped_file_path, 'rb') as f_in:
+            unzipped_file_path = os.path.join(output_folder, file_name.replace('.gz', ''))
+            with open(unzipped_file_path, 'wb') as f_out:
+                with gzip.open(f_in, 'rb') as gz:
+                    shutil.copyfileobj(gz, f_out)
+        
+        # delete original .gz file
+        os.remove(gzipped_file_path)
+        print(f"File downloaded and unzipped successfully. Saved at: {unzipped_file_path}")
+    else:
+        print(f"Failed to download file. HTTP Status Code: {response.status_code}")
+
+
+def main():
+    # The URLs of the files to download
+    urls = [
+        'https://datasets.imdbws.com/name.basics.tsv.gz',
+        'https://datasets.imdbws.com/title.akas.tsv.gz',
+        'https://datasets.imdbws.com/title.basics.tsv.gz',
+        'https://datasets.imdbws.com/title.crew.tsv.gz',
+        'https://datasets.imdbws.com/title.episode.tsv.gz',
+        'https://datasets.imdbws.com/title.principals.tsv.gz',
+        'https://datasets.imdbws.com/title.ratings.tsv.gz',
+    ]
+
+    # Path to the 'database' folder where the unzipped file will be stored
+    output_folder = '../database'
+
+    # Ensure the 'database' directory exists
+    os.makedirs(output_folder, exist_ok=True)
+
+    for url in urls:
+        download_unzip(url, output_folder)
+
+main()
