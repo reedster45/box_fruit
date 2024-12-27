cb84ed57a91b41d4967c7e0196c466b6c7af9483
Tyhuis <reed.tyhuis@KingstonHSC.ca>
2024-12-27 12:39:45 -0500

    horizontal scrolling & randomize

--- public/views.js
+++ public/views.js
@@ -22,3 +22,100 @@ sidebarToggle.addEventListener('click', function () {
   const isOpen = sidebar.classList.contains('show');
   localStorage.setItem('sidebarOpen', isOpen);
 });
+
+
+
+
+
+
+
+
+
+
+// horizontal scrolling (home page)
+function scrollLUM() {
+  console.log('left');
+  document.getElementById('scroll-containerUM').scrollBy({
+      left: -250,
+      behavior: 'smooth'
+  });
+}
+
+function scrollRUM() {
+  console.log('right');
+  document.getElementById('scroll-containerUM').scrollBy({
+      left: 250,
+      behavior: 'smooth'
+  });
+}
+
+
+function scrollLUS() {
+  console.log('left');
+  document.getElementById('scroll-containerUS').scrollBy({
+      left: -250,
+      behavior: 'smooth'
+  });
+}
+
+function scrollRUS() {
+  console.log('right');
+  document.getElementById('scroll-containerUS').scrollBy({
+      left: 250,
+      behavior: 'smooth'
+  });
+}
+
+
+
+function scrollLMM() {
+  console.log('left');
+  document.getElementById('scroll-containerMM').scrollBy({
+      left: -250,
+      behavior: 'smooth'
+  });
+}
+
+function scrollRMM() {
+  console.log('right');
+  document.getElementById('scroll-containerMM').scrollBy({
+      left: 250,
+      behavior: 'smooth'
+  });
+}
+
+
+function scrollLMS() {
+  console.log('left');
+  document.getElementById('scroll-containerMS').scrollBy({
+      left: -250,
+      behavior: 'smooth'
+  });
+}
+
+function scrollRMS() {
+  console.log('right');
+  document.getElementById('scroll-containerMS').scrollBy({
+      left: 250,
+      behavior: 'smooth'
+  });
+}
+
+
+
+
+function scrollLR() {
+  console.log('left');
+  document.getElementById('scroll-containerR').scrollBy({
+      left: -250,
+      behavior: 'smooth'
+  });
+}
+
+function scrollRR() {
+  console.log('right');
+  document.getElementById('scroll-containerR').scrollBy({
+      left: 250,
+      behavior: 'smooth'
+  });
+}
