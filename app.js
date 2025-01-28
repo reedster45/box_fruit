3331bbfdfb33a9b65aac9ce83a53556b91c33a71
Tyhuis <reed.tyhuis@KingstonHSC.ca>
2025-01-28 08:28:02 -0500

    remove jackett and arr for 2embed

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 
 
 import { fileURLToPath } from 'url';
-import { readFile } from 'fs' // Async version of readFile
 import express from 'express';
-import WebTorrent from 'webtorrent';
 import path from 'path';
 import bodyParser from 'body-parser'
-import sqlite3 from 'sqlite3';
 import axios from 'axios';
 import dotenv from 'dotenv';
 
@@ -21,17 +18,21 @@ const port = 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// setup WebTorrent client & handle errors
-const client = new WebTorrent();
-
 // TMDb Base URL and API Key
 const TMDB_BASE_URL = 'https://api.themoviedb.org/3';
 const TMDB_IMAGE_BASE_URL = 'https://image.tmdb.org/t/p';
 const TMDB_API_KEY = process.env.TMDB_API_KEY;
 
+
+
+// import WebTorrent from 'webtorrent';
+
+// setup WebTorrent client & handle errors
+// const client = new WebTorrent();
+
 // JACKETT Base URL and API key
-const JACKETT_URL = 'http://127.0.0.1:9117/api/v2.0/indexers/all/results';
-const JACKETT_KEY = process.env.JACKETT_KEY;
+// const JACKETT_URL = 'http://127.0.0.1:9117/api/v2.0/indexers/all/results';
+// const JACKETT_KEY = process.env.JACKETT_KEY;
 
 
 
@@ -45,46 +46,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Middleware
 app.use(bodyParser.json());
-
-
-
-
-
-
-
-// // Open SQLite database (it will create the database file if it doesn't exist)
-// const db = new sqlite3.Database(db_path, (err) => {
-//   if (err) {
-//     console.error('Error opening database:', err.message);
-//   } else {
-//     console.log('Connected to the SQLite database.');
-//   }
-// });
-
-// // Function to initialize the database by executing the create_database.sql file
-// function initializeDatabase() {
-//   // Asynchronously read the SQL statements from the create_database.sql file
-//   readFile('./src/create_database.sql', 'utf-8', (err, sql) => {
-//     if (err) {
-//       console.error('Error reading the SQL file:', err.message);
-//       return;
-//     }
-
-//     // Execute the SQL to create tables (if not already created)
-//     db.exec(sql, (err) => {
-//       if (err) {
-//         console.error('Error executing SQL from file:', err.message);
-//       } else {
-//         console.log('Database tables checked/created from SQL file.');
-//       }
-//     });
-//   });
-// }
-
-// // init database - should I leave it here?
-// initializeDatabase();
-
-// fetches top ten movie torrents from JACKETT API indexers
 
 
 
@@ -399,20 +360,7 @@ app.get('/favs', (req, res) => {
 // page for streaming media
 // Movie Details Route
 app.get('/streammovie/:id', async (req, res) => {
-  let magnet_link = req.query.link;
-  const magnet_links = await getTorrents(req.query.query, req.query.type);
   const streamId = req.params.id; // Get the movie ID from the URL
-
-  if(!magnet_link) {
-    if (magnet_links[0]) {
-      magnet_link = encodeURIComponent(magnet_links[0].magnetLink);
-    } else {
-      magnet_link = 'magnet:?xt=urn:btih:6A30BCACA0F4C822FC5A581F73A239DC353B3352&dn=Hammy%20El%20Hamster%20DVDRip%20Spanish%20Ac3%202008&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce&tr=udp%3A%2F%2Ftracker.bittor.pw%3A1337%2Fannounce&tr=udp%3A%2F%2Fpublic.popcorn-tracker.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.dler.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fexodus.desync.com%3A6969&tr=udp%3A%2F%2Fopen.demonii.com%3A1337%2Fannounce';
-    }
-  }
-
-  // console.log(magnet_links);
-  // console.log(`magnet link: ${magnet_link}`);
 
   try {
     // Fetch detailed movie information
@@ -424,7 +372,7 @@ app.get('/streammovie/:id', async (req, res) => {
     const backdropPath = movie.backdrop_path ? `${TMDB_IMAGE_BASE_URL}/w1280${movie.backdrop_path}` : null;
 
     // Render movie details page
-    res.render('streammovie', { movie, backdropPath, imageBaseUrl: TMDB_IMAGE_BASE_URL, magnet: magnet_link, magnet_links, });
+    res.render('streammovie', { movie, backdropPath, imageBaseUrl: TMDB_IMAGE_BASE_URL, });
 
   } catch (error) {
     console.error('Error fetching movie details:', error.message);
@@ -433,234 +381,29 @@ app.get('/streammovie/:id', async (req, res) => {
 });
 
 app.get('/streamtv/:tv_id/season/:season_number/episode/:episode_number', async (req, res) => {
-  let magnet_link = req.query.link;
   const { tv_id, season_number, episode_number } = req.params;
-  const query = `${req.query.query} s${String(season_number).padStart(2, '0')}e${String(episode_number).padStart(2, '0')}`;
-  const magnet_links = await getTorrents(query, req.query.type);
-
-  if(!magnet_link) {
-    if (magnet_links[0]) {
-      magnet_link = encodeURIComponent(magnet_links[0].magnetLink);
-    } else {
-      magnet_link = 'magnet:?xt=urn:btih:AC53297780F7A36F74D02D09D8D6DD2E018440B7&dn=%5BSpanish%5D.Solo.Leveling.ReAwakening.2024.1080p.TeleSync.X264.COLLECTiVE&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce&tr=udp%3A%2F%2Ftracker.bittor.pw%3A1337%2Fannounce&tr=udp%3A%2F%2Fpublic.popcorn-tracker.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.dler.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fexodus.desync.com%3A6969&tr=udp%3A%2F%2Fopen.demonii.com%3A1337%2Fannounce';
-    }
-  }
-
-  // console.log(magnet_links);
-  // console.log(query);
 
   try {
     // Fetch the episode details
-    const episodeResponse = await axios.get(`${TMDB_BASE_URL}/tv/${tv_id}/season/${season_number}/episode/${episode_number}`, {
-      params: { api_key: TMDB_API_KEY },
-    });
-
+    const episodeResponse = await axios.get(`${TMDB_BASE_URL}/tv/${tv_id}/season/${season_number}/episode/${episode_number}`, { params: { api_key: TMDB_API_KEY }, });
     const episode = episodeResponse.data;
 
     // Fetch the TV show details for the header (optional)
-    const tvResponse = await axios.get(`${TMDB_BASE_URL}/tv/${tv_id}`, {
-      params: { api_key: TMDB_API_KEY },
-    });
-
+    const tvResponse = await axios.get(`${TMDB_BASE_URL}/tv/${tv_id}`, { params: { api_key: TMDB_API_KEY }, });
     const tvshow = tvResponse.data;
 
     // Fetch details for the selected season
     const seasonResponse = await axios.get(`${TMDB_BASE_URL}/tv/${tv_id}/season/${season_number}`, { params: { api_key: TMDB_API_KEY }, });
-
     const season = seasonResponse.data;
 
     // Render movie details page
-    res.render('streamtv', { episode, tvshow, season, imageBaseUrl: TMDB_IMAGE_BASE_URL, magnet: magnet_link, availableSeasons: tvshow.seasons, magnet_links, });
+    res.render('streamtv', { episode, tvshow, season, imageBaseUrl: TMDB_IMAGE_BASE_URL, availableSeasons: tvshow.seasons, });
 
   } catch (error) {
     console.error('Error fetching movie details:', error.message);
     res.status(500).send('Error fetching movie details');
   }
 });
-
-app.get('/torrent_id', (req, res) => {
-  const magnetLink = decodeURIComponent(req.query.magnet);
-  const infoHash = extractInfoHash(magnetLink); // Extract the infoHash from the magnet link
-
-  // Check if the torrent is already added by infoHash
-  let curr_torrent;
-  const existingTorrent = client.torrents.some((torrent) => {
-    curr_torrent = torrent;
-    return torrent.infoHash.toLowerCase() === infoHash.toLowerCase();
-  });
-  console.log(infoHash);
-  console.log(existingTorrent);
-
-  if (existingTorrent) {
-    console.log(`Torrent already added: ${curr_torrent.infoHash}`);
-    return handleTorrent(curr_torrent, res);
-  }
-
-  client.add(magnetLink, (torrent) => {
-    console.log(`Torrent added: ${torrent.infoHash}`);
-    handleTorrent(torrent, res);
-  });
-
-  function handleTorrent(torrent, res) {
-    // print name of each file in torrent
-    torrent.files.forEach(file => {
-      console.log(file.name);
-    });
-  
-
-    // get video file from torrent
-    const videoExtensions = ['.mp4', '.mkv', '.avi', '.mov', '.flv', '.wmv', '.webm', '.vob', '.mpg', '.mpeg', '.m4v', '.3gp', '.ts', '.m2ts', '.f4v', '.rm', '.rmvb', '.divx', '.ogv', '.asf', '.mxf', '.bik', '.drc', '.qt'];
-    const file = torrent.files.find(file => videoExtensions.some(ext => file.name.endsWith(ext)));
-    let filetype;
-    if (file) {
-      filetype = file.name.split('.').pop();
-      console.log(`File type: .${filetype}`);
-    } else {
-      return res.status(404).send("No matching video file found.");
-    }
-  
-    res.setHeader('Content-Type', `video/${filetype}`);
-  
-    // Get the Range header
-    const range = req.headers.range;
-    if (!range) {
-      return res.status(400).send("Requires Range header");
-      // const stream = file.createReadStream();
-      // stream.pipe(res);
-    }
-    
-    console.log('Range request received:', range);
-    
-    const matches = range.replace(/bytes=/, "").split("-");
-    const start = parseInt(matches[0], 10);
-    const fileSize = file.length;
-    const end = matches[1] ? parseInt(matches[1], 10) : fileSize - 1;
-  
-    // ensure range is valid
-    if (start >= fileSize || end >= fileSize) {
-      return res.status(416).send('Requested Range Not Satisfiable');
-    }
-  
-    const chunkSize = end - start + 1;
-    if (!res.headersSent) {
-      console.log("sending headers...");
-      res.writeHead(206, {
-        "Content-Range": `bytes ${start}-${end}/${fileSize}`,
-        "Accept-Ranges": "bytes",
-        "Content-Length": chunkSize,
-        "Content-Type": `video/${filetype}`
-      });
-    }
-  
-    // stream content in range
-    console.log(start, end);
-    const stream = file.createReadStream({ start, end });
-    stream.pipe(res);
-  
-  
-    // Handle stream events
-    stream.on('open', () => {
-      console.log('Streaming data now...');
-    });
-  
-    stream.on('end', () => {
-      console.log('Stream ended.');
-    });
-  
-    stream.on('error', (err) => {
-      console.error('Stream error:', err);
-      if (!res.headersSent) {
-        res.status(500).send('Internal Server Error');
-      }
-    });
-  
-    res.on('close', () => {
-      console.log('Client closed the connection.');
-      stream.destroy();  // Clean up the stream if client disconnects
-    });
-  
-  
-  
-    // Handle stream progress
-    torrent.on('download', (bytes) => {
-      // console.log(`Downloaded ${bytes} bytes`);
-    });
-  
-    torrent.on('done', () => {
-      console.log('Download complete');
-    });
-  }
-});
-
-// Helper function to extract infoHash from the magnet link
-function extractInfoHash(magnetLink) {
-  const regex = /xt=urn:btih:([a-f0-9]{40})/i;
-  const match = magnetLink.match(regex);
-  return match ? match[1] : null;
-}
-
-
-
-
-
-
-
-
-// idea for TV: (use title and IMDB id?)
-//    search for season first s0
-//    if there is a season
-//      search through for specific ep file
-//    if there is no season
-//      search for s01e01
-
-// OR
-
-// USE SONARR
-// add series (to specific folder)
-// query season/episode
-// get top ten results
-// remove series
-
-async function getTorrents(query, type) {
-  let category;
-
-  if (type == 'movie') {
-    category = '2000,2010,2020'; // Movies category
-  } else if (type == 'tv') {
-    category = '5000,5020,5050' // TV category
-  }
-
-  try {
-      const response = await axios.get(JACKETT_URL, {
-          params: {
-              apikey: JACKETT_KEY,
-              Query: query,
-              Category: category
-          }
-      });
-
-
-      const results = response.data.Results
-          .filter(result => result.MagnetUri) // Keep only results with a magnet link
-          .sort((a, b) => (b.Seeders || 0) - (a.Seeders || 0)) // Sort by seeders in descending order
-          .slice(0, 10) // Keep the top 10 results
-          .map(result => ({
-              title: result.Title,
-              magnetLink: result.MagnetUri,
-              seeders: result.Seeders || 0,
-              leechers: result.Leechers || 0,
-              tracker: result.Tracker
-          }));
-
-      return results;
-      
-  } catch (error) {
-      console.error('Error fetching data from Jackett:', error.message);
-      return json({ error: 'Failed to fetch data from Jackett' });
-  }
-}
-
-
 
 
 
